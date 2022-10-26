81a828a67ed1b929427692587da57edcfda9ef19
Jose Socorro <jcarlossoorro@gmail.com>
2022-10-25 22:19:28 -0400

    App completed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDatabase");
+mongoose.connect("mongodb+srv://jose:1234@cluster0.conr5ph.mongodb.net/blogDatabase");
 
 const entrySchema = new mongoose.Schema({
   title: String,
